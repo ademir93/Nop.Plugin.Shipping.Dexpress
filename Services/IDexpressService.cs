d7a3e2913ff66a1100801900a75652ea36f90bda
Demir <demir.agovic@omnistreak.com>
2025-10-25 23:22:02 +0200

    refresh gitignore

--- Services/IDexpressService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Nop.Services.Shipping;
-
-namespace Nop.Plugin.Shipping.Dexpress.Services;
-
-public interface IDexpressService
-{
-    Task<GetShippingOptionResponse> GetRatesAsync(GetShippingOptionRequest shippingOptionRequest);
-}
